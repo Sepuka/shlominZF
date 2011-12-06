0efad95209749cfa5fb002601735af4f29a27102
Maksim Shlomin <maksim.shlomin@gmail.com>
2011-12-06 11:47:09 +0400

    исправлен вызов конфигурации проекта

--- application/controllers/ArticleController.php
+++ application/controllers/ArticleController.php
@@ -9,22 +9,22 @@ class ArticleController extends Zend_Controller_Action
     protected $_articleModel    = null;
     protected $_config          = null;
 
-	/**
-	 * Обработка вызовов несуществующих действий
-	 *
-	 * @param string $method
-	 * @param array $args
-	 */
-	public function __call($method, $args)
-	{
-	    $this->getResponse()->setHttpResponseCode(404);
-	    $this->_helper->layout->setLayout('404');
-	}
+    /**
+    * Обработка вызовов несуществующих действий
+    *
+    * @param string $method
+    * @param array $args
+    */
+    public function __call($method, $args)
+    {
+        $this->getResponse()->setHttpResponseCode(404);
+	$this->_helper->layout->setLayout('404');
+    }
 
     public function init()
     {
         $this->_articleModel = new Application_Model_Articles();
-        $this->_config = new Application_Model_MemcachedConfig(CONFIG_FILE, APPLICATION_ENV);
+        $this->_config = Application_Model_MemcachedConfig::getInstance();
     	$this->getResponse()->setHeader('Content-Type', 'text/html; charset=UTF-8');
     }
 
